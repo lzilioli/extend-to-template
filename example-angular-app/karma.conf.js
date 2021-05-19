56d08a2a681c7ce106cbbb4860d8764b483587bb
Luke Zilioli <elzee08@gmail.com>
2021-05-19 18:24:22 -0400

    example app to own folder

--- example-angular-app/karma.conf.js
+++ example-angular-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-extends-template'),
+      dir: require('path').join(__dirname, './coverage/example-angular-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- example-angular-app/src/app/app.component.spec.ts
+++ example-angular-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-extends-template'`, () => {
+  it(`should have as title 'example-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-extends-template');
+    expect(app.title).toEqual('example-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-extends-template app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('example-angular-app app is running!');
   });
 });
